1e359d61d55660f14bd537d9456fe4dda1470a6a
Elrich De Villiers <Edevilliers9789@conestogac.on.ca>
2021-01-24 23:07:28 -0500

    Add code to create and find sum of array

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int[] numArray;
+            int arraySum = 0;
+
+            try
+            {
+                Console.WriteLine("Enter numeric array values seperated by commas. Ex 5,3,6,5");
+                var values = Console.ReadLine();
+
+                // Split the string into an array
+                string[] stringArray = values.Split(',');
+
+                // Convert the string array into a List of integers
+                foreach (string num in stringArray)
+                {
+                    if (string.IsNullOrEmpty(num))
+                    {
+                        numbers.Add(0);
+                    }
+                    else
+                    {
+                        if (!Int32.TryParse(num, out int number))
+                        {
+                            throw new Exception("Input values must be numbers");
+                        }
+                        numbers.Add(number);
+                    }
+
+                }
+
+                // Convert the list to an array in order to show that I can find the sum of an array
+                numArray = numbers.ToArray();
+
+                foreach (int num in numArray)
+                {
+                    arraySum = arraySum + num;
+                }
+
+                Console.WriteLine($"The sum of the array is {arraySum}");
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Something went Wrong: {e}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
